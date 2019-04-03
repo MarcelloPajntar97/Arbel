1d724d605d2b0eb4c8b0b6d826157ca30fd52b3e
Guy97 <marcello.pajntar@ied.edu>
2019-04-03 15:59:30 +0200

    Backend-risolto collegamento molti a molti. Due restanti

--- database/migrations/2019_03_13_213143_create_subjects_table.php
+++ database/migrations/2019_03_13_213143_create_subjects_table.php
@@ -15,6 +15,8 @@ class CreateSubjectsTable extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('subjectName');
             $table->integer('credits');
             $table->string('lessonArgument');
