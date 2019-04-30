0a940b688c0038f70cb7bde1a4abdaffe7b94b6e
Simone Ghisu <s.gizzy@live.it>
2019-04-30 21:14:42 +0200

    BACKEND - Subjects Seed
    Aggiunte in SubjectTableSeeder le materie relative ai 3 anni di Sound Design, CG Animation e Graphic Design

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -562,5 +562,545 @@ class SubjectsTableSeeder extends Seeder
           'class_id'=>9
       ]);
 
+      //Sound Design
+
+      App\Subject::create([
+          'subjectName'=>'Storia dell\'arte contemporanea',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Teoria e analisi del cinema',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecnologie e applicazioni digitali',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Informazione per l\'arte: mezzi e metodi',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'TPPF 1',
+          'credits'=>4,
+          'totHours'=>36,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di ripresa',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sound Design 1',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Audio e Mixaggio 1',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Semiotica dell\'arte',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Storia della musica contemporanea 1',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Antropologia culturale - cultura pop',
+          'credits'=>2,
+          'totHours'=>30,
+          'class_id'=>10
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Acustica',
+          'credits'=>2,
+          'totHours'=>16,
+          'class_id'=>10
+      ]);
+
+
+      App\Subject::create([
+          'subjectName'=>'TPPF 2',
+          'credits'=>6,
+          'totHours'=>45,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sound Design 2',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Audio e mixaggio 2',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Fenomenologia delle arti contemporanee',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Processi e tecniche per lo spettacolo multimediale',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sistemi interattivi 1',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Storia della musica contemporanea 2',
+          'credits'=>6,
+          'totHours'=>40,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche performative per le arti visive',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>11
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sociologia della comunicazione',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>11
+      ]);
+
+
+      App\Subject::create([
+          'subjectName'=>'Sound design 3',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>12
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sistemi interattivi 2',
+          'credits'=>60,
+          'totHours'=>6,
+          'class_id'=>12
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Storia della musica contemporanea 3',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>12
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Diritto, legislazione ed economia dello spettacolo',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>12
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione multimediale',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>12
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sound Design 4',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>12
+      ]);
+
+      //CG
+      App\Subject::create([
+          'subjectName'=>'Storia dell\'arte contemporanea',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Teoria e analisi del cinema',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Informazione per l\'arte: mezzi e metodi',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'TPPF',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di animazione digitale 1',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Cinematografia',
+          'credits'=>4,
+          'totHours'=>60,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione multimediale 1',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Culture digitali',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Semiotica dell\'arte',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>34
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di modellazione digitale - Computer 3D 1',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>34
+      ]);
+
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di animazione digitale 2',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione multimediale 2',
+          'credits'=>12,
+          'totHours'=>120,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Fenomenologia delle arti contemporanee',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sistemi interattivi',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Regia',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di modellazione digitale - Computer 3D 2',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sociologia della comunicazione',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Anatomia dell\'immagine',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>35
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Metodologia progettuale della comunicazione visiva',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>35
+      ]);
+
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di animazione digitale 3',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>36
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Teoria e metodo dei Mass Media',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>36
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione multimediale 3',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>36
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sistemi interattivi 2',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>36
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di modellazione digitale - Computer 3D 3',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>36
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione multimediale 4',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>36
+      ]);
+
+      //GRAPHIC DESIGN
+      App\Subject::create([
+          'subjectName'=>'Storia dell\'arte contemporanea',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Storia del disegno e della grafica d\'arte',
+          'credits'=>8,
+          'totHours'=>48,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Metodologia della progettazione',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Computer Graphic 1',
+          'credits'=>12,
+          'totHours'=>120,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Elementi di grafica editoriale',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Graphic Design 1',
+          'credits'=>12,
+          'totHours'=>120,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'TPPF',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Semiotica dell\'arte',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>13
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Video grafica',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Graphic design 2',
+          'credits'=>10,
+          'totHours'=>100,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Fotografia',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Fenomenologia delle arti contemporanee',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Brand Design',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Metodologia progettuale della comunicazione visiva',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Sociologia della comunicazione',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Storia del cinema e del video',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche dei procedimenti a stampa',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Tecniche di modellazione digitale - computer 3D',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>14
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Teoria e metodo dei Mass Media',
+          'credits'=>6,
+          'totHours'=>36,
+          'class_id'=>15
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Graphic design 3',
+          'credits'=>12,
+          'totHours'=>120,
+          'class_id'=>15
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Digital Video',
+          'credits'=>6,
+          'totHours'=>60,
+          'class_id'=>15
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Progettazione grafica',
+          'credits'=>8,
+          'totHours'=>80,
+          'class_id'=>15
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Design Management',
+          'credits'=>4,
+          'totHours'=>30,
+          'class_id'=>15
+      ]);
+
+      App\Subject::create([
+          'subjectName'=>'Allestimento degli spazi espositivi',
+          'credits'=>4,
+          'totHours'=>40,
+          'class_id'=>15
+      ]);
+
     }
 }
