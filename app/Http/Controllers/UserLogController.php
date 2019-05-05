f5ce76c092b2aaed4e63a33d9cbfea8c45d6b476
Guy97 <marcello.pajntar@ied.edu>
2019-05-05 02:53:10 +0200

    Backend-API al login da mobile l'utente visualizzerà i suoi corsi e materie ad esso associate

--- app/Http/Controllers/UserLogController.php
+++ app/Http/Controllers/UserLogController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\Subject;
+use App\ClassModel;
 
 class UserLogController extends Controller
 {
@@ -16,22 +18,19 @@ class UserLogController extends Controller
    * @return \Illuminate\Http\Response
    */
   public function login() {
-      if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+      if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
           $user = Auth::user();
+          $subjects = auth()->user()->subjects()->with('class')->get();
           $success['token'] =  $user->createToken('MyApp')-> accessToken;
           $success['id'] =  $user->id;
           $success['name'] =  $user->name;
           $success['surname'] =  $user->surname;
           $success['email'] =  $user->email;
+          $success['dataDetails'] =  $subjects;
           return response()->json(['success' => $success], $this-> successStatus);
       }
-      else{
+      else {
           return response()->json(['error'=>'Unauthorised'], 401);
       }
-  }
-
-  public function details() {
-        $user = Auth::user();
-        return response()->json(['success' => $user], $this-> successStatus);
   }
 }
