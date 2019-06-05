c9c385a59ca2264a1aef50b9e8529fe1f831a759
federicabrieda <federica.brieda@ied.edu>
2019-06-06 00:14:04 +0200

    UI-studentDetail
    quasi completata la grafica

--- resources/views/studentDetail.blade.php
+++ resources/views/studentDetail.blade.php
@@ -2,15 +2,16 @@
 @include('elements.nav')
 @section('content')
 
-<div class = "container"><h3 class = "coursesTitle">{{ __('DETTAGLIO STUDENTE:') }}</h3></div>
+<div class="container">
+  <h3 class="detailTitle">{{ __('DETTAGLIO STUDENTE:') }}</h3>
+</div>
 <div class="container studentprofile">
-
-
-  <div class = "row">
-    <div class = "col-md-3">
+  <div class="row">
+    <div class="col-md-3">
       <div class="profileImg">
         <div class="card mb-4">
-          <img src="/storage/avatars/{{ $students->avatar }}" style = "width:170px; height:170px; float:left; border-radius:50%; margin-left:25px; margin-bottom:25px;" />
+
+          <img class="imgstudent" src="{{ $students->avatar }}"/>
           <div class="card-body">
             <div class="card-test">
               <h6 class="studentdati">Data di nascita: {{ $students->birthday }}</h6>
@@ -27,78 +28,60 @@
             <div class="card-test">
               <h6 class="studentdati">Assenze: 2%</h6>
             </div>
-            <div class="d-flex justify-content-between align-items-center">
-            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class = "col-md-8">
-      <div class = "row">
-        <div class = "col">
-          <h4 class = "nameUser">{{ $students->name }} {{ $students->surname }}</h4> <br>
-          <h6 class = "mailUser" >{{ $students->email }}</h6>
-
-            <ul class="nav nav-pills container switchContainer row row-centered  " id="myTabJust" role="tablist">
-              <li class="nav-item col-md-3 col-centered ">
-                <a class="nav-link active studentBox" id="student-tab-just" data-toggle="tab" href="#student-just" role="tab" aria-controls="student-just"
-                aria-selected="true">Media</a>
-              </li>
-              <li class="nav-item col-md-3">
-                <a class="nav-link testBox" id="test-tab-just" data-toggle="tab" href="#test-just" role="tab" aria-controls="test-just"
-                aria-selected="false">Assenze</a>
-              </li>
-              <li class="nav-item col-md-3">
-                <a class="nav-link activityBox" id="activity-tab-just" data-toggle="tab" href="#activity-just" role="tab" aria-controls="activity-just"
-                aria-selected="false">Note</a>
-              </li>
-            </ul>
-            <div class="tab-content card pt-5" id="myTabContentJust">
-              <div class="tab-pane fade show active" id="student-just" role="tabpanel" aria-labelledby="studenti-tab-just">
-                <div class = "container">
-                  <div class="row-centered studentContainer">
-                    <div class="row ">
-                      <div class = "col-md-4 ">
-                        Data
-                      </div>
-                      <div class = "col-md-3 text-left">
-                        Tipologia
-                      </div>
-                      <div class = "col-md-3 text-centered">
-                        Valutazione
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="row-centered classContainer">
-                    <div class="row">
-                      <div class = "col-md-4 ">
-                        22/02/2019
-                      </div>
-                      <div class = "col-md-3 text-centered">
-                        Fenomelogia delle arti contemporanee
-                      </div>
-                      <div class = "col-md-3 text-centered">
-                        27
-                      </div>
-                    </div>
-                  </div>
-                  <!-- <button type="submit" class="btn  btn-primary  studentSubmitButton">{{ __('SALVA') }}</button> -->
-                  <button type="submit" class="btn  btn-primary  studentSubmitButton">{{ __('AGGIUNGI VOTO') }}</button>
-                </div>
+    <div class="col-md-8">
+      <div class="row">
+        <div class="col">
+          <h4 class="nameUser">{{ $students->name }} {{ $students->surname }}</h4> <br>
+          <h6 class="mailUser" >{{ $students->email }}</h6>
+          <form>
+            <h3 class="detailTitle">{{ __('Media:') }}</h3>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <input type="text" class="form-control" placeholder="Inserisci la data" id="inputData">
               </div>
-              <div class="tab-pane fade" id="test-just" role="tabpanel" aria-labelledby="test-tab-just">
-                DA MODIFICARE
+              <div class="form-group col-md-4">
+                <select id="inputTipologia" class="form-control"  placeholder="Tipologia" >
+                  <option selected> Tipologia</option>
+                  <option>Orale</option>
+                  <option>Scritto</option>
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <input type="text" class="form-control" placeholder="Valutazione" id="inputValutazione">
               </div>
             </div>
-            <div class="tab-pane fade" id="test-just" role="tabpanel" aria-labelledby="test-tab-just">
-              DA MODIFICARE
+          </form>
+
+          <h3 class="detailTitle">{{ __('Assenze:') }}</h3>
+          @foreach ($students as $studentsdetail)
+          <div class="row-centered">
+            <div class="row">
+              <div class="col-md-2 textleft">
+                02/01/2019
+              </div>
+              <div class="col-md-1 text-centered">
+                2%
+              </div>
             </div>
           </div>
-        </div>
+          @endforeach
+
+          <h3 class="detailTitle">{{ __('Note:') }}</h3>
+          <div class="form-row">
+            <div class="form-group col-md-8">
+              <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Aggiungi una nota"></textarea>
+            </div>
+          </div>
+          <button type="submit" class="btn  btn-primary  studentSubmitButton">{{ __('SALVA') }}</button>
+        </form>
       </div>
     </div>
   </div>
+</div>
 </div>
 @endsection
